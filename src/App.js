421b80855263ca301eee4f160cb78b3110ba0408
AdarshVp77 <adarshvjayan77@gmail.com>
2024-04-13 19:47:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Empform from "./components/EmpForm";
+import Login from "./components/Login";
+import Mains from "./components/Main";
+import Signup from "./components/Signup";
+import AdminLogin from "./components/AdminLogin";
+import AdminSignup from "./components/AdminSignup";
+import UpdateEmp from "./components/UpdateEmp";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<Login />}></Route>
+        <Route path={"/admin/login"} element={<AdminLogin />}></Route>
+        <Route path={"/admin/signup"} element={<AdminSignup />}></Route>
+        <Route path={"/signup"} element={<Signup />}></Route>
+
+        <Route path="/empapp" element={<Mains child={<Dashboard />} />} />
+        <Route path="/home" element={<Mains child={<Home />} />} />
+        <Route path="/form" element={<Mains child={<Empform />} />} />
+        <Route path="/form/:id" element={<Mains child={<Empform />} />} />
+        <Route path="/update" element={<Mains child={<UpdateEmp />} />} />
+        <Route path="/update/:id" element={<Mains child={<UpdateEmp />} />} />
+      </Routes>
     </div>
   );
 }
